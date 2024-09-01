28177a0cbd467bf9c576bbc7a9b3f614a7216456
anp <1367621299@qq.com>
2024-09-01 09:11:42 +0800

    这次提交是完全提交了他的项目源码

---
+++ README.md
@@ -0,0 +1,1 @@
+这个是完全抄于https://github.com/WuBingzheng/build-lua-in-rust,准备写一下标准库啥的，稍微完善一下
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+
+mod value;
+mod bytecode;
+mod lex;
+mod parse;
+mod lua_vm;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {} script", args[0]);
+        return;
+    }
+    let file = File::open(&args[1]).unwrap();
+
+    let proto = parse::load(BufReader::new(file));
+    lua_vm::vm::ExeState::new().execute(&proto, &Vec::new());
 }
